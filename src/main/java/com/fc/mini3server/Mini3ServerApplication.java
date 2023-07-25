ee498481d1c30e3fd937f540aae0ceda1a2ef723
Ussu1112 <ussu1112@naver.com>
2023-07-25 15:21:25 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/fc/mini3server/Mini3ServerApplication.java
@@ -0,0 +1,13 @@
+package com.fc.mini3server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mini3ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mini3ServerApplication.class, args);
+    }
+
+}
