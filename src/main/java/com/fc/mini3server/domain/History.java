5cee135c7def913f9c49307094feda169727cb74
atsunsetree <atsunsetree529@gmail.com>
2023-07-26 22:04:00 +0900

    feat #2: History 모델 생성

---
+++ src/main/java/com/fc/mini3server/domain/History.java
@@ -0,0 +1,29 @@
+package com.fc.mini3server.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Entity(name = "history_tb")
+public class History {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    private User userId;
+
+    @ManyToOne(optional = false)
+    private Schedule scheduleId;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+}
