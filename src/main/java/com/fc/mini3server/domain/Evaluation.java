ea48004f6152c4e5bcb6d649ecee77b91ffb35e6
atsunsetree <atsunsetree529@gmail.com>
2023-07-26 21:44:11 +0900

    feat #2: Schedule 모델 생성

---
+++ src/main/java/com/fc/mini3server/domain/Evaluation.java
@@ -0,0 +1,7 @@
+package com.fc.mini3server.domain;
+
+public enum Evaluation {
+    STANDBY,
+    APPROVED,
+    REJECTED
+}
---
+++ src/main/java/com/fc/mini3server/domain/Schedule.java
@@ -0,0 +1,51 @@
+package com.fc.mini3server.domain;
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Entity(name = "schedule_tb")
+public class Schedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    private User userId;
+
+    @ManyToOne(optional = false)
+    private Hospital hospitalId;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false)
+    private Category category;
+
+    @Column(nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(nullable = false)
+    private LocalDateTime endDate;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false)
+    private Evaluation evaluation;
+
+    private String reason;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
