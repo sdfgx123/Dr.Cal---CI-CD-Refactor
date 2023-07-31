3969073bbe4056e41164a898533634a2a83a16b5
sdfgx123 <sdfgx123@naver.com>
2023-07-31 15:27:20 +0900

    feat #10: register function added on controller

--- src/main/java/com/fc/mini3server/controller/UserController.java
+++ src/main/java/com/fc/mini3server/controller/UserController.java
@@ -2,20 +2,31 @@ package com.fc.mini3server.controller;
 
 import com.fc.mini3server._core.utils.ApiUtils;
 import com.fc.mini3server.domain.User;
+import com.fc.mini3server.dto.UserRequestDTO;
 import com.fc.mini3server.dto.UserResponseDTO;
 import com.fc.mini3server.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/user")
 public class UserController {
+
     private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<ApiUtils.ApiResult<User>> register(@RequestBody @Valid UserRequestDTO.registerDTO registerDTO, Errors errors) {
+        userService.registerNewUser(registerDTO);
+        return ResponseEntity.ok(ApiUtils.success(null));
+    }
 
     @GetMapping("/users/{id}")
     public ResponseEntity<?> findById(@PathVariable Long id){
