d08699e4258b7f39df0bec9e624a0663aec5bba2
sdfgx123 <sdfgx123@naver.com>
2023-08-02 00:07:21 +0900

    feat #11: add login and logout func on the Controller

--- src/main/java/com/fc/mini3server/controller/UserController.java
+++ src/main/java/com/fc/mini3server/controller/UserController.java
@@ -7,6 +7,7 @@ import com.fc.mini3server.dto.UserResponseDTO;
 import com.fc.mini3server.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
@@ -25,6 +26,23 @@ public class UserController {
     @PostMapping("/register")
     public ResponseEntity<ApiUtils.ApiResult<User>> register(@RequestBody @Valid UserRequestDTO.registerDTO registerDTO, Errors errors) {
         userService.registerNewUser(registerDTO);
+        return ResponseEntity.ok(ApiUtils.success(null));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiUtils.ApiResult<String>> login(@RequestBody @Valid UserRequestDTO.loginDTO loginDTO, Errors errors) {
+        String jwtToken = userService.login(loginDTO);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.AUTHORIZATION, jwtToken);
+
+        log.info("JWT Token provided : " + jwtToken);
+
+        return ResponseEntity.ok().headers(headers).body(null);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<ApiUtils.ApiResult<String>> logout() {
         return ResponseEntity.ok(ApiUtils.success(null));
     }
 
