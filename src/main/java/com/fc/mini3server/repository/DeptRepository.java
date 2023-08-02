945ca335439dd8ba53134434e45f17433ea779ef
ussu1112 <ussu1112@naver.com>
2023-08-02 19:15:55 +0900

    refactor #24: 병원 아이디가 없는 경우에 빈 값으로 전송

--- src/main/java/com/fc/mini3server/repository/DeptRepository.java
+++ src/main/java/com/fc/mini3server/repository/DeptRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface DeptRepository extends JpaRepository<Dept, Long> {
-
     List<Dept> findAllByHospitalId(Long id);
 }
