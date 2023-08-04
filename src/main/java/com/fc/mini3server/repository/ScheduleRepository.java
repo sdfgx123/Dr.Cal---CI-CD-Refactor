b27ed0e74a31cc35c476b9dc0a17aa1e4dafdb13
atsunsetree <atsunsetree529@gmail.com>
2023-08-05 07:06:27 +0900

    Feat #16: 대기 중 취소를 위한 레포지토리 추가

--- src/main/java/com/fc/mini3server/repository/ScheduleRepository.java
+++ src/main/java/com/fc/mini3server/repository/ScheduleRepository.java
@@ -6,10 +6,16 @@ import com.fc.mini3server.domain.Schedule;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
     Page<Schedule> findByCategoryIsOrderById(CategoryEnum category, Pageable pageable);
     Page<Schedule> findByEvaluation(EvaluationEnum evaluation, Pageable pageable);
 
+    @Modifying
+    @Query("UPDATE schedule_tb s SET s.evaluation = 'CANCELED' WHERE s.id = :scheduleId")
+    void updateEvaluationToCanceled(@Param("scheduleId") Long scheduleId);
 }
