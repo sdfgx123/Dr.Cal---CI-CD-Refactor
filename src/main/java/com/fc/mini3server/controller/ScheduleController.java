1b853b1b3c4dacb98442df07c259088ec1eaa9aa
atsunsetree <atsunsetree529@gmail.com>
2023-08-04 11:51:16 +0900

    feat #16: 연차 등록 POST 컨트롤러 추가

--- src/main/java/com/fc/mini3server/controller/ScheduleController.java
+++ src/main/java/com/fc/mini3server/controller/ScheduleController.java
@@ -1,14 +1,20 @@
 package com.fc.mini3server.controller;
 
 import com.fc.mini3server._core.utils.ApiUtils;
+import com.fc.mini3server.domain.CategoryEnum;
+import com.fc.mini3server.domain.Hospital;
+import com.fc.mini3server.domain.Schedule;
+import com.fc.mini3server.domain.User;
+import com.fc.mini3server.dto.ScheduleRequestDTO;
 import com.fc.mini3server.service.ScheduleService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Tag(name = "Schedule", description = "캘린더 API")
 @RequiredArgsConstructor
@@ -21,5 +27,11 @@ public class ScheduleController {
     @GetMapping("/")
     public ResponseEntity<?> getApprovedSchedules(Pageable pageable) {
         return ResponseEntity.ok(ApiUtils.success(scheduleService.getApprovedSchedule(pageable)));
+    }
+
+    @PostMapping("/create/annual")
+    public ResponseEntity<?> createAnnualSchedule(@RequestBody ScheduleRequestDTO.createAnnualDTO createAnnualDTO, @AuthenticationPrincipal User user, Hospital hospital) {
+        Schedule createdSchedule = scheduleService.createSchedule(createAnnualDTO, user, hospital);
+        return ResponseEntity.ok(ApiUtils.success(null));
     }
 }
