5c9d995ca3ad8322b4f7f84a20e29f6cac40e342
Ussu1112 <ussu1112@naver.com>
2023-10-24 23:37:09 +0900

    refactor: 병원별 의사 목록 QueryDsl로 변경

--- src/main/java/com/fc/mini3server/repository/UserRepository.java
+++ src/main/java/com/fc/mini3server/repository/UserRepository.java
@@ -1,16 +1,16 @@
 package com.fc.mini3server.repository;
 
-import com.fc.mini3server.domain.*;
+import com.fc.mini3server.domain.Hospital;
+import com.fc.mini3server.domain.StatusEnum;
+import com.fc.mini3server.domain.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 
-
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryCustom {
 
     User findByEmail(String email);
 
@@ -21,5 +21,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Page<User> findByHospitalAndStatusIs(Hospital hospital, StatusEnum status, Pageable pageable);
 
-    List<User> findAllByHospitalAndAuthAndLevelIn(Hospital hospital, AuthEnum auth, List<LevelEnum> level);
 }
