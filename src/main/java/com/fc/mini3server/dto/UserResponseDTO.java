e79a8e2e4643a606613cd461d8e6d3c717b0f1a5
sdfgx123 <sdfgx123@naver.com>
2023-10-28 17:52:59 +0900

    feat #93: 마이페이지 API DTO 전체 구조 변경

--- src/main/java/com/fc/mini3server/dto/UserResponseDTO.java
+++ src/main/java/com/fc/mini3server/dto/UserResponseDTO.java
@@ -1,6 +1,7 @@
 package com.fc.mini3server.dto;
 
 import com.fc.mini3server.domain.User;
+import com.fc.mini3server.domain.Work;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,35 +11,44 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Builder
 public class UserResponseDTO {
 
-    private Long id;
-    private Long empNo;
-    private String name;
-    private String email;
-    private String phone;
-    private Long hospitalId;
-    private Long deptId;
-    private String level;
-    private String auth;
-    private String status;
-    private int annual;
-    private int duty;
-    private String profileImageUrl;
-    private LocalDate hireDate;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    @Builder
+    public static class MyPageDTO {
+        private Long id;
+        private Long empNo;
+        private String name;
+        private String email;
+        private String phone;
+        private Long hospitalId;
+        private Long deptId;
+        private String level;
+        private String auth;
+        private String status;
+        private int annual;
+        private int duty;
+        private String profileImageUrl;
+        private LocalDate hireDate;
+        private LocalDateTime workStart;
+        private int flag;
+        private LocalDateTime createdAt;
+        private LocalDateTime updatedAt;
 
+        public static MyPageDTO of(User user, Work work) {
+            LocalDateTime workStart = LocalDateTime.of(2023, 10, 1, 0, 0);
+            int flag;
 
-
-    public static UserResponseDTO of(User user){
-        if (user == null) {
-            return new UserResponseDTO();
-        }
-        return new UserResponseDTO(
+            if (work != null) {
+                workStart = work.getStartTime();
+                flag = 1;
+            }
+            else {
+                flag = 0;
+            }
+            return new MyPageDTO(
                 user.getId(),
                 user.getEmpNo(),
                 user.getName(),
@@ -53,7 +63,38 @@ public class UserResponseDTO {
                 user.getDuty(),
                 user.getProfileImageUrl(),
                 user.getHiredDate(),
+                workStart,
+                flag,
                 user.getCreatedAt(),
-                user.getUpdatedAt());
+                user.getUpdatedAt()
+            );
+        }
     }
+
+
+
+
+
+//    public static UserResponseDTO of(User user){
+//        if (user == null) {
+//            return new UserResponseDTO();
+//        }
+//        return new UserResponseDTO(
+//                user.getId(),
+//                user.getEmpNo(),
+//                user.getName(),
+//                user.getEmail(),
+//                user.getPhone(),
+//                user.getHospital().getId(),
+//                user.getDept().getId(),
+//                user.getLevel().name(),
+//                user.getAuth().name(),
+//                user.getStatus().name(),
+//                user.getAnnual(),
+//                user.getDuty(),
+//                user.getProfileImageUrl(),
+//                user.getHiredDate(),
+//                user.getCreatedAt(),
+//                user.getUpdatedAt());
+//    }
 }
