366b6d5cb945120aed6ecf8006f2505908edb6bd
sdfgx123 <sdfgx123@naver.com>
2023-10-31 21:23:56 +0900

    feat #94: 나의 근무관리 조회 JPA 코드 수정

--- src/main/java/com/fc/mini3server/repository/WorkRepository.java
+++ src/main/java/com/fc/mini3server/repository/WorkRepository.java
@@ -6,12 +6,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface WorkRepository extends JpaRepository<Work, Long> {
-    Work findByUserIdAndStartTimeBetween(Long userId, LocalDateTime start, LocalDateTime end);
-    List<Work> findByUserIdAndStartTimeBetween(User user, LocalDateTime start, LocalDateTime end);
-    Page<Work> findByUserAndStartTimeBetween(User user, LocalDate startOfweek, LocalDate endOfweek, Pageable pageable);
+    Work findFirstByUserIdAndStartTimeBetween(Long userId, LocalDateTime start, LocalDateTime end);
+    List<Work> findAllByUserIdAndStartTimeBetween(Long userId, LocalDateTime start, LocalDateTime end);
+    Page<Work> findByUserAndStartTimeBetween(User user, LocalDateTime startOfweek, LocalDateTime endOfweek, Pageable pageable);
+    Optional<Work> findTopByUserIdOrderByStartTimeDesc(Long userId);
 }
