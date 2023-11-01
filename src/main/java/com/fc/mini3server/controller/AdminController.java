a386ddf820bf3fef240f2870020635870021aa5f
Ussu1112 <ussu1112@naver.com>
2023-11-01 17:09:54 +0900

    refactor: 연차관리, 당직변경관리 조회 쿼리 QueryDsl로 변경

--- src/main/java/com/fc/mini3server/controller/AdminController.java
+++ src/main/java/com/fc/mini3server/controller/AdminController.java
@@ -10,9 +10,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.data.web.SortDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -48,32 +46,28 @@ public class AdminController {
 
     @Operation(summary = "유저 권한 변경", description = "USER -> ADMIN, ADMIN -> USER")
     @PostMapping("/users/{id}/auth")
-    public ResponseEntity<?> editAuth(@PathVariable Long id, @RequestBody editAuthDTO requestDTO){
+    public ResponseEntity<?> editAuth(@PathVariable Long id, @RequestBody editAuthDTO requestDTO) {
         adminService.updateUserAuth(id, requestDTO);
         return ResponseEntity.ok(ApiUtils.success(null));
     }
 
     @Operation(summary = "유저 권한 승인", description = "NOTAPPROVED -> APPROVED")
     @PostMapping("/users/{id}/approve")
-    public ResponseEntity<?> userApprove(@PathVariable Long id){
+    public ResponseEntity<?> userApprove(@PathVariable Long id) {
         adminService.approveUser(id);
         return ResponseEntity.ok(ApiUtils.success(null));
     }
 
     @Operation(summary = "유저 재직 상태 변경", description = "APPROVED -> RETIRED")
     @PostMapping("/users/{id}/retire")
-    public ResponseEntity<?> userRetire(@PathVariable Long id){
+    public ResponseEntity<?> userRetire(@PathVariable Long id) {
         adminService.retireUser(id);
         return ResponseEntity.ok(ApiUtils.success(null));
     }
 
     @Operation(summary = "연차 신청 관리", description = "디폴트: 오래된순 + state 처리 완료된건 뒤로 정렬")
     @GetMapping("/annual")
-    public ResponseEntity<?> findAnnualList(@PageableDefault(size = 10)
-            @SortDefault.SortDefaults({
-                    @SortDefault(sort = "updatedAt", direction = Sort.Direction.ASC),
-                    @SortDefault(sort = "id", direction = Sort.Direction.ASC)
-            }) Pageable pageable){
+    public ResponseEntity<?> findAnnualList(@PageableDefault(size = 10) Pageable pageable) {
         final Page<Schedule> scheduleList = adminService.findAnnualList(pageable);
         return ResponseEntity.ok(ApiUtils.success(
                 scheduleList.getTotalPages(),
@@ -83,11 +77,7 @@ public class AdminController {
 
     @Operation(summary = "당직 변경 관리", description = "디폴트: 오래된순 + state 처리 완료된건 뒤로 정렬")
     @GetMapping("/duty")
-    public ResponseEntity<?> findDutyList(@PageableDefault(size = 10)
-              @SortDefault.SortDefaults({
-                      @SortDefault(sort = "updatedAt", direction = Sort.Direction.ASC),
-                      @SortDefault(sort = "id", direction = Sort.Direction.ASC)
-              }) Pageable pageable){
+    public ResponseEntity<?> findDutyList(@PageableDefault(size = 10) Pageable pageable) {
         final Page<Schedule> scheduleList = adminService.findDutyList(pageable);
         return ResponseEntity.ok(ApiUtils.success(
                 scheduleList.getTotalPages(),
@@ -96,28 +86,28 @@ public class AdminController {
 
     @Operation(summary = "스케줄 승인/반려", description = "STANDBY -> APPROVED, STANDBY -> REJECTED")
     @PostMapping("/{scheduleId}/evaluation")
-    public ResponseEntity<?> editEvaluation(@PathVariable Long scheduleId, @RequestBody editEvaluationDTO requestDTO){
+    public ResponseEntity<?> editEvaluation(@PathVariable Long scheduleId, @RequestBody editEvaluationDTO requestDTO) {
         adminService.updateScheduleEvaluation(scheduleId, requestDTO);
         return ResponseEntity.ok(ApiUtils.success(null));
     }
 
     @Operation(summary = "병원 별 의사 목록", description = "병원Id를 가지고 당직등록을 위한 사람을 찾는다.")
     @GetMapping("/hospitalUsers")
-    public ResponseEntity<?> findUserListByHospital(@RequestParam(required = false, defaultValue = "") List<LevelEnum> levelList){
+    public ResponseEntity<?> findUserListByHospital(@RequestParam(required = false, defaultValue = "") List<LevelEnum> levelList) {
         List<User> userList = adminService.findAllUserListByHospitalIdAdmin(levelList);
         return ResponseEntity.ok(ApiUtils.success(UserListByHospitalIdDTO.listOf(userList)));
     }
 
     @Operation(summary = "당직 추가", description = "선택한 당직인원을 실제 당직인원으로 추가한다.")
     @PostMapping("/{userId}/createDuty")
-    public ResponseEntity<?> createDuty(@PathVariable Long userId, @RequestBody createDutyAdminDTO requestDTO){
+    public ResponseEntity<?> createDuty(@PathVariable Long userId, @RequestBody createDutyAdminDTO requestDTO) {
         adminService.createDuty(userId, requestDTO);
         return ResponseEntity.ok(ApiUtils.success(null));
     }
 
     @Operation(summary = "당직 삭제", description = "요청한 날짜에 당직을 삭제한다.")
     @PostMapping("/{scheduleId}/deleteDuty")
-    public ResponseEntity<?> deleteDuty(@PathVariable Long scheduleId){
+    public ResponseEntity<?> deleteDuty(@PathVariable Long scheduleId) {
         adminService.deleteDuty(scheduleId);
         return ResponseEntity.ok(ApiUtils.success(null));
     }
--- src/main/java/com/fc/mini3server/repository/ScheduleRepository.java
+++ src/main/java/com/fc/mini3server/repository/ScheduleRepository.java
@@ -2,10 +2,7 @@ package com.fc.mini3server.repository;
 
 import com.fc.mini3server.domain.CategoryEnum;
 import com.fc.mini3server.domain.EvaluationEnum;
-import com.fc.mini3server.domain.Hospital;
 import com.fc.mini3server.domain.Schedule;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -15,14 +12,16 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
-public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+public interface ScheduleRepository extends JpaRepository<Schedule, Long>, ScheduleRepositoryCustom {
 
-    @Query("SELECT s FROM schedule_tb s where s.hospital = :hospital AND s.category = :category ORDER BY CASE WHEN s.evaluation = 'STANDBY' THEN 1 ELSE 2 END")
-    Page<Schedule> findByHospitalAndCategoryIsOrderById(@Param("hospital") Hospital hospital, @Param("category") CategoryEnum category, Pageable pageable);
     List<Schedule> findByEvaluationAndUserHospitalId(EvaluationEnum evaluation, Long hospitalId);
+
     List<Schedule> findByHospitalIdAndEvaluationAndCategoryAndStartDateIsLessThanEqualAndEndDateIsGreaterThanEqual(Long hospitalId, EvaluationEnum evaluation, CategoryEnum category, LocalDate startDate, LocalDate endDate);
+
     Optional<Schedule> findByHospitalIdAndEvaluationAndCategoryAndStartDate(Long HospitalId, EvaluationEnum evaluation, CategoryEnum category, LocalDate startDate);
+
     Optional<Schedule> findByHospitalIdAndEvaluationAndCategoryAndStartDateAndEndDate(Long hospitalId, EvaluationEnum evaluationEnum, CategoryEnum category, LocalDate chooseDate, LocalDate chooseDate1);
+
     List<Schedule> findByUserId(Long userId);
 
     @Modifying
