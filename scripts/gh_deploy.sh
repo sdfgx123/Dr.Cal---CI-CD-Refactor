25ff5cf26ebf5c172eece93ef58e3d6464acfc0e
atsunsetree <atsunsetree529@gmail.com>
2023-11-07 10:23:21 +0900

    fix #96: 경로 수정

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -5,7 +5,7 @@ JAR_NAME=$(basename $BUILD_JAR)
 echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/mini3-server/deploy.log
 
 echo "> build 파일 복사" >> /home/ubuntu/mini3-server/deploy.log
-DEPLOY_PATH=/home/ubuntu/sictak/
+DEPLOY_PATH=/home/ubuntu/mini3-server/
 cp $BUILD_JAR $DEPLOY_PATH
 
 echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/mini3-server/deploy.log
